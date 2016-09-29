7023d729e7b0decf49a6aeaae31f2e3a17c56cce
Arelia Jones <arelia.jones@careerbuilder.com>
2016-09-29 17:38:38 -0400

    Setup mailer

--- app/mailers/standup_mailer.rb
+++ app/mailers/standup_mailer.rb
@@ -1,2 +1,8 @@
 class StandupMailer < ApplicationMailer
+  default from: 'setme@setme.com'
+
+  def daily(standup)
+    @standup = standup
+    mail(to: @standup.team.standup_email, subject: 'Daily Scrupdate')
+  end
 end
