64b0ab67344de1217c8ee86e573c86660de6fc06
Arelia Jones <arelia.jones@careerbuilder.com>
2016-09-29 14:00:52 -0400

    Modify controllers

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,64 +1,41 @@
 class GroupsController < ApplicationController
-  before_action :set_group, only: [:show, :edit, :update, :destroy]
+  before_action :set_group, only: [:show, :update, :destroy]
 
   # GET /groups
-  # GET /groups.json
   def index
     @groups = Group.all
+
+    render json: @groups
   end
 
   # GET /groups/1
-  # GET /groups/1.json
   def show
-  end
-
-  # GET /groups/new
-  def new
-    @group = Group.new
-  end
-
-  # GET /groups/1/edit
-  def edit
+    render json: @group
   end
 
   # POST /groups
-  # POST /groups.json
   def create
     @group = Group.new(group_params)
 
-    respond_to do |format|
-      if @group.save
-        format.html { redirect_to @group, notice: 'Group was successfully created.' }
-        format.json { render :show, status: :created, location: @group }
-      else
-        format.html { render :new }
-        format.json { render json: @group.errors, status: :unprocessable_entity }
-      end
+    if @group.save
+      render json: @group, status: :created, location: @group
+    else
+      render json: @group.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /groups/1
-  # PATCH/PUT /groups/1.json
   def update
-    respond_to do |format|
-      if @group.update(group_params)
-        format.html { redirect_to @group, notice: 'Group was successfully updated.' }
-        format.json { render :show, status: :ok, location: @group }
-      else
-        format.html { render :edit }
-        format.json { render json: @group.errors, status: :unprocessable_entity }
-      end
+    if @group.update(group_params)
+      render json: @group
+    else
+      render json: @group.errors, status: :unprocessable_entity
     end
   end
 
   # DELETE /groups/1
-  # DELETE /groups/1.json
   def destroy
     @group.destroy
-    respond_to do |format|
-      format.html { redirect_to groups_url, notice: 'Group was successfully destroyed.' }
-      format.json { head :no_content }
-    end
   end
 
   private
@@ -67,7 +44,7 @@ class GroupsController < ApplicationController
       @group = Group.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+    # Only allow a trusted parameter "white list" through.
     def group_params
       params.require(:group).permit(:name, :team_id)
     end
--- app/controllers/team_members_controller.rb
+++ app/controllers/team_members_controller.rb
@@ -1,64 +1,41 @@
 class TeamMembersController < ApplicationController
-  before_action :set_team_member, only: [:show, :edit, :update, :destroy]
+  before_action :set_team_member, only: [:show, :update, :destroy]
 
   # GET /team_members
-  # GET /team_members.json
   def index
     @team_members = TeamMember.all
+
+    render json: @team_members
   end
 
   # GET /team_members/1
-  # GET /team_members/1.json
   def show
-  end
-
-  # GET /team_members/new
-  def new
-    @team_member = TeamMember.new
-  end
-
-  # GET /team_members/1/edit
-  def edit
+    render json: @team_member
   end
 
   # POST /team_members
-  # POST /team_members.json
   def create
     @team_member = TeamMember.new(team_member_params)
 
-    respond_to do |format|
-      if @team_member.save
-        format.html { redirect_to @team_member, notice: 'Team member was successfully created.' }
-        format.json { render :show, status: :created, location: @team_member }
-      else
-        format.html { render :new }
-        format.json { render json: @team_member.errors, status: :unprocessable_entity }
-      end
+    if @team_member.save
+      render json: @team_member, status: :created, location: @team_member
+    else
+      render json: @team_member.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /team_members/1
-  # PATCH/PUT /team_members/1.json
   def update
-    respond_to do |format|
-      if @team_member.update(team_member_params)
-        format.html { redirect_to @team_member, notice: 'Team member was successfully updated.' }
-        format.json { render :show, status: :ok, location: @team_member }
-      else
-        format.html { render :edit }
-        format.json { render json: @team_member.errors, status: :unprocessable_entity }
-      end
+    if @team_member.update(team_member_params)
+      render json: @team_member
+    else
+      render json: @team_member.errors, status: :unprocessable_entity
     end
   end
 
   # DELETE /team_members/1
-  # DELETE /team_members/1.json
   def destroy
     @team_member.destroy
-    respond_to do |format|
-      format.html { redirect_to team_members_url, notice: 'Team member was successfully destroyed.' }
-      format.json { head :no_content }
-    end
   end
 
   private
@@ -67,7 +44,7 @@ class TeamMembersController < ApplicationController
       @team_member = TeamMember.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+    # Only allow a trusted parameter "white list" through.
     def team_member_params
       params.require(:team_member).permit(:first_name, :last_name, :email, :groups_id)
     end
