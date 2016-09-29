d9add50006c53741154fafeb529b5af6c9364037
Arelia Jones <arelia.jones@careerbuilder.com>
2016-09-29 18:47:33 -0400

    Configure mailer

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,15 @@ module Scrumlord
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.action_mailer.smtp_settings = {
+      address: config.smtp.sender_address,
+      domain: config.smtp.sender_domain,
+      port: config.smtp.port,
+      user_name: config.smtp.user_name,
+      password: config.smtp.password,
+      starttls: config.smtp.starttls
+    }
+
   end
 end
