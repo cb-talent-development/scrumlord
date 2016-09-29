5bf154a363815d6ccea43f7175b6834be93ad14e
ElliottAYoung <elliott.a.young@gmail.com>
2016-09-29 19:08:49 -0400

    Build Team Member Gravatar URL methods in Model + validations

--- app/models/team_member.rb
+++ app/models/team_member.rb
@@ -2,4 +2,14 @@ class TeamMember < ApplicationRecord
   has_many :standup_team_members
   has_many :standups, through: :standup_team_members
   belongs_to :group
+
+  validates :email, :first_name, :last_name, presence: true
+
+  def gravatar_url
+    Gravatar.new(email).url(size: 300)
+  end
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
