3959601913237e68139cf1e0b3bb2453e9ee57a9
Arelia Jones <arelia.jones@careerbuilder.com>
2016-09-29 15:40:09 -0400

    Scope API routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,13 @@ Rails.application.routes.draw do
 
   devise_for :teams
 
-  resources :standups, except: [:new, :edit] do
-    resources :standup_team_members, path: 'team_members', except: [:new, :edit]
-  end
+  scope '/api' do
+    resources :standups, except: [:new, :edit] do
+      resources :standup_team_members, path: 'team_members', except: [:new, :edit]
+    end
 
-  resources :groups, except: [:new, :edit] do
-    resources :team_members, except: [:new, :edit]
+    resources :groups, except: [:new, :edit] do
+      resources :team_members, except: [:new, :edit]
+    end
   end
 end
