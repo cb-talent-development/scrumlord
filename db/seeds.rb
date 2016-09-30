4d59c1828e80f773dfae24e39b4bf7544edb1a11
ElliottAYoung <elliott.a.young@gmail.com>
2016-09-29 21:03:23 -0400

    Fix Team Member Emails

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,41 +17,41 @@ devs = Group.create({
 TeamMember.create({
   first_name: "Alex",
   last_name: "Tharp",
-  email: "@careerbuilder.com",
+  email: "alex.tharp@careerbuilder.com",
   group: devs
 })
 
 TeamMember.create({
   first_name: "Arelia",
   last_name: "Jones",
-  email: "@careerbuilder.com",
+  email: "arelia.jones@careerbuilder.com",
   group: devs
 })
 
 TeamMember.create({
   first_name: "Elliott",
   last_name: "Young",
-  email: "@careerbuilder.com",
+  email: "elliott.young@careerbuilder.com",
   group: devs
 })
 
 TeamMember.create({
   first_name: "Justin",
   last_name: "Thompson",
-  email: "@careerbuilder.com",
+  email: "justin.thompson@careerbuilder.com",
   group: pos
 })
 
 TeamMember.create({
   first_name: "Stephanie",
   last_name: "Gaspary",
-  email: "@careerbuilder.com",
+  email: "stephanie.gaspary@careerbuilder.com",
   group: pos
 })
 
 TeamMember.create({
   first_name: "Kristy",
   last_name: "Cullinane",
-  email: "@careerbuilder.com",
+  email: "kristy.cullinane@careerbuilder.com",
   group: pos
 })
