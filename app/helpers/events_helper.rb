d83e3b00f6958fc58636e84fc3dc93fa56077789
ElliottAYoung <elliott.a.young@gmail.com>
2016-09-29 20:29:56 -0400

    Add Events Helper

---
+++ app/helpers/events_helper.rb
@@ -0,0 +1,13 @@
+module EventsHelper
+
+  def events_today
+    get_events["data"]["Events"]
+  end
+
+  private
+
+  def get_events
+    JSON.parse(Excon.get('http://history.muffinlabs.com/date').body)
+  end
+
+end
