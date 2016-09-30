e5ed26ada888d2cabb1153a7ae5616fb7aea1831
ElliottAYoung <elliott.a.young@gmail.com>
2016-09-29 21:24:51 -0400

    Build Standups Controller

--- app/controllers/standups_controller.rb
+++ app/controllers/standups_controller.rb
@@ -1,2 +1,31 @@
 class StandupsController < AuthenticatedApplicationController
+  include EventsHelper
+
+  def index
+    @standups = Standup.all
+  end
+
+  def new
+    @groups = Group.all
+    @standup = Standup.new
+  end
+
+  def create
+    @standup = Standup.new(standup_params)
+
+    if @standup.save
+      flash[:success] = "Standup Email Sent!"
+      daily(@standup)
+      redirect_to dashboard
+    else
+      flash[:errors] = @standup.errors.full_messages
+      redirect_to new_standup_path
+    end
+  end
+
+  private
+
+  def standup_params
+    params.require(:standup).permit(:date, :comments, :meetings, :event_of_the_day, :team_id, standup_team_member_attributes: [])
+  end
 end
